618656a8313b6c99099b5b62b02d50d5f65e04d7
Nam Mai <nam.nhmai@gmail.com>
2024-09-18 12:41:52 -0700

    Add Heroku dependencies

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn investment_project.wsgi --log-file -
--- investment_project/settings.py
+++ investment_project/settings.py
@@ -21,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$6zx85w-9w_284&*2#*ln2(30fasea9ml6@+x%9_%xl#7xx4_3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://compound-interest-visualizer-efb4a179a2bc.herokuapp.com/',
+    'https://compoundinterestvisualizer.com'
+]
 
 
 # Application definition
