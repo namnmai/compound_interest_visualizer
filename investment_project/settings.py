efda411e35fae7599043aff9f12558e707840af8
Nam Mai <nam.nhmai@gmail.com>
2024-09-18 12:46:55 -0700

    Collect staticfiles

--- investment_project/settings.py
+++ investment_project/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),  # Where your static files are located during development
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic will collect files for deployment
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
